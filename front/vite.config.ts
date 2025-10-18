7ef38167c3029e4355dc58572ac8a38d1ca56535
rq-pepepepopopo-at-321899771753 <timeflies325@gmail.com>
2025-10-19 00:11:39 +0900

    💻環境構築_Rails+Vite+React+tailwind+daisyUI

---
+++ front/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// @ts-ignore
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
+})
