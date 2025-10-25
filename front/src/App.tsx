3f0fbc94d89e65078e867b57c6ac53fde357e4d9
rq-pepepepopopo-at-321899771753 <timeflies325@gmail.com>
2025-10-26 00:37:10 +0900

    daisyUI導入

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,12 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", "lofi");
+  },[])
   const [count, setCount] = useState(0)
 
   return (
@@ -16,7 +19,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className='text-red-500'>Vite + React</h1>
+      <h1 className='error'>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
