edcf2da070ec58421bafff32deb7f7a6adbfab20
rq-pepepepopopo-at-321899771753 <timeflies325@gmail.com>
2025-10-28 21:23:12 +0900

    React router導入

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import { routes } from "./routes";
 import './index.css'
-import Home from './pages/Home.tsx'
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter(routes);
+
+const root = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(root).render(
   <StrictMode>
-    <Home />
+    <RouterProvider router={router} />,
   </StrictMode>,
 )
