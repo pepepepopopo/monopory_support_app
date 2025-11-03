aec7fac3d9bb784eb428ddea04b550d850ed9328
向井航平 <timeflies325@gmail.com>
2025-11-03 19:34:41 +0900

    ゲームID入力画面を作成

--- front/src/components/Modal/JoinGameModal.tsx
+++ front/src/components/Modal/JoinGameModal.tsx
@@ -1,13 +1,31 @@
-import Modal from "./Modal";
-import JoinGame from "../../pages/games/JoinGame";
+import { useRef } from "react";
 
-const JoinGameModal = ({ isModalOpen, handleCloseModal
-}: {isModalOpen: boolean; handleCloseModal: () => void;}) => {
+const JoinGameModal = () => {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
   return(
-    <Modal isModalOpen={isModalOpen} handleCloseModal={handleCloseModal}>
-      <JoinGame />
-    </Modal>
+    <>
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+      <button className="btn btn-primary w-full h-16" onClick={()=>dialogRef.current?.showModal()}>→　ゲームに参加</button>
+      <dialog id="my_modal_1" className="modal" ref={dialogRef}>
+        <div className="modal-box">
+          <form>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">ゲームIDを入力してください</legend>
+              <input type="text" className="input input-primary w-full" placeholder="ゲームIDを入力" />
+              <button type="submit" className="btn btn-block">参加</button>
+            </fieldset>
+          </form>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+    </>
   )
 }
 
-export default JoinGameModal
+export default JoinGameModal;
