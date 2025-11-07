8a873ef01d9cf63d225e8fd6513adc8ffd8ea1f0
rq-pepepepopopo-at-321899771753 <timeflies325@gmail.com>
2025-11-08 01:02:40 +0900

    playerコントろらー作成

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   namespace :api do
     resources :games, only: [:index, :create, :update, :destroy]
+    resources :users, only: [:index, :create, :update, :destroy]
     resources :logs, only: [:index, :create, :update, :destroy]
 end
 end
