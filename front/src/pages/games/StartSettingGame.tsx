706cec739c868b3f2f918fc117eddaabcdc2c490
rq-pepepepopopo-at-321899771753 <timeflies325@gmail.com>
2025-11-10 00:28:29 +0900

    newgamesettingレイアウト変更

--- front/src/pages/games/StartSettingGame.tsx
+++ front/src/pages/games/StartSettingGame.tsx
@@ -1,7 +1,25 @@
+import { Link } from "react-router";
+
 const StartSettingGame = () => {
   return(
     <>
-    Hello
+      <Link to="/games" className="btn mb-3">
+        戻る
+      </Link>
+      <div className="flex flex-col gap-6 rounded-xl border glass px-6 [&:last-child]:pb-6">
+        <div className="grid auto-rows-min gap-1.5 pt-6 ">
+          <div className="leading-none">ゲームの初期設定を行います</div>
+        </div>
+        <div className="space-y-2">
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">ゲームID</legend>
+            <input type="text" className="input input-primary" placeholder="名前を入力" />
+          </fieldset>
+        </div>
+        <button
+          type="button"
+          className="btn btn-block btn-primary">ゲームを作成</button>
+      </div>
     </>
   )
 }
