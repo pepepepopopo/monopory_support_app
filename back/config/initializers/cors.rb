e12131cef2d8d3769ac0d32e94c7e75cf53511f0
向井航平 <timeflies325@gmail.com>
2026-02-01 18:16:12 +0900

    cross 設定

--- back/config/initializers/cors.rb
+++ back/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8000", "https://www.本番環境のURLがあるならここにも追加"
+    origins "localhost:8000", "https://monopory-buddy-front.onrender.com"
 
     resource "*",
       headers: :any,
