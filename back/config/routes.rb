d1b276e52fa30af8fcb56d65f3130ae44807dbb0
向井航平 <timeflies325@gmail.com>
2026-03-11 21:40:05 +0900

    health

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
+  get "/health" => proc {
+    ActiveRecord::Base.connection.execute("SELECT 1")
+    [200, { "Content-Type" => "text/plain" }, ["ok"]]
+  }
   namespace :api do
     resources :games, param: :join_token, only: [:show, :create, :destroy] do
       resources :players, only: [:index]
